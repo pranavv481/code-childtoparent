e8992be3c2b015539708210f618a669ad1d6a402
pranavv481 <pranavv481@gmail.com>
2020-05-23 13:03:05 +0530

    from child to parent

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import ParentComponent from './components/ParentComponent';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <ParentComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
